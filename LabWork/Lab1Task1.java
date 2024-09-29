0f7a418e5a346e1a8c027812b1a3e6b4b37043ea
Neel Patkar <149313087+neelpatkar007@users.noreply.github.com>
2024-09-29 02:32:49 +0100

    Lab1Task1.java

---
+++ LabWork/Lab1Task1.java
@@ -0,0 +1,15 @@
+// Task 1
+public class Lab1Task1 {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		int num4 = 4;
+
+		System.out.println(num1 + " " + num2 + " " + num3 + " " + num4);
+		System.out.print(num1 + " " + num2 + " " + num3 + " " + num4);
+
+	}
+
+}
